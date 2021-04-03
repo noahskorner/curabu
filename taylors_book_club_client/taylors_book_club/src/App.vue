e87d0839986507fa2190d76fcb86633ced61c80a
Noah Korner <noahskorner@gmail.com>
2021-04-03 16:35:45 -0500

    Initialize Tailwind and themes.

--- taylors_book_club_client/taylors_book_club/src/App.vue
+++ taylors_book_club_client/taylors_book_club/src/App.vue
@@ -1,30 +1,22 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="min-h-screen" id="app">
+    <!-- Theme -->
+    <teleport to="head">
+      <link rel="stylesheet" type="text/css" href="/themes/light-theme.css" />
+    </teleport>
   </div>
-  <router-view />
 </template>
 
 <style>
+html,
+body {
+  overflow-x: hidden;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
 }
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+button:focus {
+  outline: 0 !important;
 }
 </style>
