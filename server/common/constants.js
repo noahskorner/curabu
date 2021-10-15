64432345125044bf6a1ec67d4d563342023e7400
Noah Korner <noahskorner@gmail.com>
2021-10-14 21:21:56 -0500

    Completed first iteration of authServer.

--- server/common/constants.js
+++ server/common/constants.js
@@ -1,9 +1,11 @@
+const usernameRegex = /^[a-z0-9-_\.]+$/;
 const emailRegex =
   /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 const accessTokenExpiration = 1800; //seconds = 15 minutes
 const refreshTokenExpiration = 15; //days
 
 module.exports = {
+  usernameRegex,
   emailRegex,
   accessTokenExpiration,
   refreshTokenExpiration,
--- server/common/functions.js
+++ server/common/functions.js
@@ -1,23 +1,42 @@
 const pool = require("../db");
 const jwt = require("jsonwebtoken");
-const { emailRegex, accessTokenExpiration } = require("../common/constants");
+const {
+  usernameRegex,
+  emailRegex,
+  accessTokenExpiration,
+  refreshTokenExpiration,
+} = require("../common/constants");
 
-const validateNewUser = async (email, password1, password2) => {
+const validateUser = async (username, email, password1, password2) => {
   const errors = [];
 
   try {
     const result = await pool.query(
-      "SELECT email FROM users WHERE email = $1 LIMIT 1",
-      [email]
+      "SELECT * FROM users WHERE email = $1 OR username = $2 LIMIT 1",
+      [email, username]
     );
 
-    if (result.rows[0]) errors.push(`User with email ${email} already exists.`);
+    if (result.rows[0])
+      errors.push("User with that username or email already exists.");
+
+    if (username.length < 5 || username.length > 25)
+      errors.push("Username must be 5 - 25 characters long.");
+
+    if (!usernameRegex.test(username))
+      errors.push(
+        "Username can only contain letters, numbers, hypens, underscores, and periods."
+      );
+
     if (!emailRegex.test(email)) errors.push("Email must be valid.");
+
     if (password1 !== password2) errors.push("Passwords do not match.");
+
     if (password1.length < 8)
       errors.push("Password must be at least 8 characters.");
+
     if (!/\d/.test(password1))
       errors.push("Password must contain at least 1 number.");
+
     if (!/[A-Z]/.test(password1))
       errors.push("Password must contain at least 1 uppercase letter.");
   } catch (error) {
@@ -35,11 +54,13 @@ const generateAccessToken = (user) => {
 };
 
 const generateRefreshToken = (user) => {
-  return jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
+  return jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, {
+    expiresIn: refreshTokenExpiration,
+  });
 };
 
 module.exports = {
-  validateNewUser,
+  validateUser,
   generateAccessToken,
   generateRefreshToken,
 };
